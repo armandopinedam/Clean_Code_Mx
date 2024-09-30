37bb63dfbf7f4c63477da5875466e17b2329d34a
Pineda Medina Jose Armando (BGSW/EXC2-MS) <pja2ga@bosch.com>
2024-09-30 08:58:36 -0600

    Code impl

---
+++ Test1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Define an array of strings
+    char *strings[] = {"Bosch", "Bangalore", "Mexico", "Canada", "India"};
+
+    // Get the size of the array
+    int size = sizeof(strings) / sizeof(strings[0]);
+
+    // Print each string in the array
+    for (int i = 0; i < size; i++) {
+        if(strings[i][0]=='B')
+        {
+            printf("%s\n", strings[i]);
+        }
+        
+    }
+
+    return 0;
+}
